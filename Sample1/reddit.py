05f89dd223db70e33c5af70ef720e72f731b81ab
TheRutubeify <elitegroup.kz@gmail.com>
2017-12-22 17:23:23 -0800

    Update reddit.py

--- Sample1/reddit.py
+++ Sample1/reddit.py
@@ -3,33 +3,20 @@
 from selenium import webdriver
 from selenium.common.exceptions import NoSuchElementException
 
-
-
-
-driver = webdriver.Chrome(executable_path='chromedriver.exe')
-# driver.implicitly_wait(1)
-
-
 s1 = 'nonfiction'
-
-
+driver = webdriver.Chrome(executable_path='chromedriver.exe')
 driver.get('https://www.reddit.com/')
-# result = False
-
 driver.find_element_by_xpath('//*[@id="search"]/input[1]').send_keys(s1)
 driver.find_element_by_xpath('//*[@id="search"]/input[2]').click()
 page = 0
 driver.implicitly_wait(1)
-
-
+#-------------------------
 arr= driver.find_elements_by_xpath("(//*[@class = 'contents'])[2]//*[@class='search-result-header']/a")
 for i in arr:
-
         print s1 in i.text
-
 page += 1
 print 'page: ', page
-
+#-------------------------
 if driver.find_element_by_xpath("(//*[@class='nextprev'])[2]/a").is_displayed():
     driver.find_element_by_xpath("(//*[@class='nextprev'])[2]/a").click()
 
