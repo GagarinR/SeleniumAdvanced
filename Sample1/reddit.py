885e6b30ff619d7b1ca49dd0f15153d72bb6839e
TheRutubeify <elitegroup.kz@gmail.com>
2017-12-22 17:22:09 -0800

    Create reddit.py

---
+++ Sample1/reddit.py
@@ -0,0 +1,59 @@
+# Basic test without Object Model
+
+from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException
+
+
+
+
+driver = webdriver.Chrome(executable_path='chromedriver.exe')
+# driver.implicitly_wait(1)
+
+
+s1 = 'nonfiction'
+
+
+driver.get('https://www.reddit.com/')
+# result = False
+
+driver.find_element_by_xpath('//*[@id="search"]/input[1]').send_keys(s1)
+driver.find_element_by_xpath('//*[@id="search"]/input[2]').click()
+page = 0
+driver.implicitly_wait(1)
+
+
+arr= driver.find_elements_by_xpath("(//*[@class = 'contents'])[2]//*[@class='search-result-header']/a")
+for i in arr:
+
+        print s1 in i.text
+
+page += 1
+print 'page: ', page
+
+if driver.find_element_by_xpath("(//*[@class='nextprev'])[2]/a").is_displayed():
+    driver.find_element_by_xpath("(//*[@class='nextprev'])[2]/a").click()
+
+    try:
+        while driver.find_element_by_xpath("//*[@class='nextprev']/a[2]").is_displayed():
+
+            arr= driver.find_elements_by_xpath("//*[@class = 'contents']//*[@class='search-result-header']/a")
+            for i in arr:
+
+                print s1 in i.text
+
+            page += 1
+            print 'page: ', page
+
+
+            driver.find_element_by_xpath("//*[@class='nextprev']/a[2]").click()
+            driver.implicitly_wait(2)
+
+    except NoSuchElementException:
+            print 'Last Page: ', page
+
+
+
+
+
+
+
