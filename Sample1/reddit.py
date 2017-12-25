4c959ad862c4a41d530b442455017fdcff01b0f7
blackbogdan <1bogdankapusta@gmail.com>
2017-12-25 12:34:07 -0800

    added webdriver and changed prints

--- Sample1/reddit.py
+++ Sample1/reddit.py
@@ -10,30 +10,30 @@ driver.find_element_by_xpath('//*[@id="search"]/input[1]').send_keys(s1)
 driver.find_element_by_xpath('//*[@id="search"]/input[2]').click()
 page = 0
 driver.implicitly_wait(1)
-#-------------------------
-arr= driver.find_elements_by_xpath("(//*[@class = 'contents'])[2]//*[@class='search-result-header']/a")
+# -------------------------
+arr = driver.find_elements_by_xpath("(//*[@class = 'contents'])[2]\
+    //*[@class='search-result-header']/a")
 for i in arr:
-        print s1 in i.text
+        print(s1 in i.text)
 page += 1
-print 'page: ', page
-#-------------------------
+print('page: ', page)
+# -------------------------
 if driver.find_element_by_xpath("(//*[@class='nextprev'])[2]/a").is_displayed():
     driver.find_element_by_xpath("(//*[@class='nextprev'])[2]/a").click()
 
     try:
         while driver.find_element_by_xpath("//*[@class='nextprev']/a[2]").is_displayed():
-
             arr= driver.find_elements_by_xpath("//*[@class = 'contents']//*[@class='search-result-header']/a")
             for i in arr:
-                print s1 in i.text
+                print(s1 in i.text)
 
             page += 1
-            print 'page: ', page
+            print('page: ', page)
             driver.find_element_by_xpath("//*[@class='nextprev']/a[2]").click()
             driver.implicitly_wait(2)
 
     except NoSuchElementException:
-            print 'Last Page: ', page
+            print('Last Page: ', page)
 
 
 
